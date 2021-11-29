3ad5d785366c0e0e7c758826fa18f1b168196b75
HighVoltage557 <88422812+HighVoltage557@users.noreply.github.com>
2021-11-29 20:02:42 +0530

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/sh
 sudo apt update -y && sudo apt upgrade -y && sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y && git clone https://github.com/xmrig/xmrig && mkdir xmrig/build && cd xmrig/build && cmake .. && make -j$(nproc)
