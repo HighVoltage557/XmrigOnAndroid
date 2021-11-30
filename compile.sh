f5555c524cceafeabdfdd426397dcb4319bba37f
HighVoltage557 <88422812+HighVoltage557@users.noreply.github.com>
2021-11-30 23:13:56 +0530

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-sudo apt update -y && sudo apt upgrade -y && sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev figlet -y && figlet -f small Cloning repository && git clone https://github.com/xmrig/xmrig && mkdir xmrig/build && cd xmrig/build && figlet -f small Compiling miner && cmake .. && make -j$(nproc) && figlet -f small Done compiling
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev figlet -y
+figlet -f small Cloning repository
+git clone https://github.com/xmrig/xmrig
+mkdir xmrig/build
+cd xmrig/build
+figlet -f small Compiling miner
+cmake ..
+make -j$(nproc)
+figlet -f small Done compiling
+sudo apt remove figlet -y
+echo Removed extra packages.
