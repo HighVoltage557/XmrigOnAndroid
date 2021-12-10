8a009e3d60aff8386bc5777fee42ed19bdc3bfd2
HighVoltage557 <88422812+HighVoltage557@users.noreply.github.com>
2021-12-10 12:25:59 +0530

    Create compilemoxmrig.sh

---
+++ compilemoxmrig.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+apt update -y
+apt upgrade -y
+apt install git proot cmake figlet -y
+figlet -f small Cloning repository
+git clone https://github.com/moneroocean/xmrig
+mkdir xmrig/build
+cd xmrig/build
+figlet -f small Compiling MO Xxmrig
+cmake -DWITH_HWLOC=OFF ..
+make -j$(nproc)
+figlet -f small Done compiling
+sudo apt remove figlet -y
+echo Removed extra packages
